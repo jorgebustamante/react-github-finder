b0c51381d578f9e946159767782bff45ac104af4
unknown <paul.jose.rodriguez@gmail.com>
2019-06-27 20:39:33 -0700

    Implementing context API

--- react-github-finder-app/src/components/users/User.js
+++ react-github-finder-app/src/components/users/User.js
@@ -1,10 +1,14 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({  match }) => {
+  const githubContext = useContext(GithubContext);
+  const { user, loading, getUser, repos, getUserRepos } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -95,12 +99,12 @@ const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
     </Fragment>
   );
 };
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired,
-  repos: PropTypes.array.isRequired
-};
+// User.propTypes = {
+//   // loading: PropTypes.bool,
+//   // user: PropTypes.object.isRequired,
+//   // getUser: PropTypes.func.isRequired,
+//   getUserRepos: PropTypes.func.isRequired,
+//   repos: PropTypes.array.isRequired
+// };
 
 export default User;
