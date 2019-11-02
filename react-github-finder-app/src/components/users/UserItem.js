f22e4042778b9459edfc97f54788598c49ff4d83
unknown <paul.jose.rodriguez@gmail.com>
2019-11-02 16:33:43 -0700

    updated border radius and css overflow issue on mobile viewport

--- react-github-finder-app/src/components/users/UserItem.js
+++ react-github-finder-app/src/components/users/UserItem.js
@@ -11,7 +11,7 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
         className='round-img'
         style={{ width: '60px' }}
       />
-      <h3>{login}</h3>
+      <h3 className='searchName'>{login}</h3>
       <div>
         <Link to={`/user/${login}`} className='btn btn-dark btn-sm my-1'>
           More
